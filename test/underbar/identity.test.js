d9fec8893f1be8ed42a454516793b5c552af1606
Jacob Sklar <jsklar.js@gmail.com>
2017-05-10 17:27:26 -0700

    added identity function and tests

--- test/underbar/identity.test.js
+++ test/underbar/identity.test.js
@@ -2,18 +2,23 @@ const _ = require('../../underbar');
 
 describe('identity()', () => {
   it('returns null if given null', () => {
-    // Your code goes here
+    expect(_.identity(null)).toBe(null);
   });
 
   it('returns the number if given a number', () => {
-    // Your code goes here
+    expect(_.identity(17)).toBe(17);
   });
 
   it('returns the same array if given an array', () => {
-    // Your code goes here
+    const array = [1, 7];
+    expect(_.identity(array)).toBe(array);
   });
 
   it('returns the same object if given an object', () => {
-    // Your code goes here
+    const object = {
+      name: 'bob',
+      age: 30
+      };
+    expect(_.identity(object)).toBe(object);
   });
 });
