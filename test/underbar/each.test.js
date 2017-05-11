c6401a81d9a0bbbc3ab4da32e98c1a9cd57557a3
Jacob Sklar <jsklar.js@gmail.com>
2017-05-11 16:03:07 -0700

    added function and test for each

--- test/underbar/each.test.js
+++ test/underbar/each.test.js
@@ -2,15 +2,42 @@ const _ = require('../../underbar');
 
 describe('each()', () => {
   it('iterates every element of an array, passing that element, its corresponding index, and the entire array to the callback', () => {
-    // Your code goes here
+    const array = ['a', 'b', 'c'];
+    let iterations = 0;
+    _.each(array, function(element, index, arr) {
+      expect(element).toEqual(arr[index]);
+      iterations += 1;
+    });
+    expect(iterations).toBe(3);
   });
 
   it('iterates every element of an array-like object, passing that element, its corresponding index, and the entire array to the callback', () => {
-    // Your code goes here
+    const arrayLike = {
+      length: 3,
+      0: 1,
+      1: 2,
+      2: 3
+    };
+    let iterations = 0;
+    _.each(arrayLike, function(element, index, arr) {
+      expect(element).toEqual(arr[index]);
+      iterations += 1;
+    });
+    expect(iterations).toBe(3);
   });
 
   it('iterates every property of an object, passing the value, the corresponding key, and the entire object to the callback', () => {
-    // Your code goes here
+    const obj = {
+      0: 1,
+      1: 2,
+      2: 3
+    };
+    let iterations = 0;
+    _.each(obj, function(element, index, arr) {
+      expect(element).toEqual(arr[index]);
+      iterations += 1;
+    });
+    expect(iterations).toBe(3);
   });
 });
 
