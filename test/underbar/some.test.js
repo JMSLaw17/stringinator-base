4413e5f09e89ed9507fb4e4d8b07091e2eac5749
Jacob Sklar <jsklar.js@gmail.com>
2017-05-12 13:33:32 -0700

    added functiona and tests for some

--- test/underbar/some.test.js
+++ test/underbar/some.test.js
@@ -2,11 +2,17 @@ const _ = require('../../underbar');
 
 describe('some()', () => {
   it('returns true if any number is odd', () => {
-    // Your code goes here
+    const array = [2, 3, 4];
+    expect(_.some(array, function(number) {
+      return number % 2 === 1;
+    })).toBe(true);
   });
 
   it('returns false if no number is odd', () => {
-    // Your code goes here
+    const array = [2, 4, 6];
+    expect(_.some(array, function(number) {
+      return number % 2 === 1;
+    })).toBe(false);
   });
 
 });
