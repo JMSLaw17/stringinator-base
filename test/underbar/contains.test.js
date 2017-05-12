1506fed62aebf6b35fa9971de98da45d4367eb67
Jacob Sklar <jsklar.js@gmail.com>
2017-05-12 13:09:50 -0700

    added function and test for contains

--- test/underbar/contains.test.js
+++ test/underbar/contains.test.js
@@ -2,19 +2,29 @@ const _ = require('../../underbar');
 
 describe('contains()', () => {
   it('returns true if an array contains the target', () => {
-    // Your code goes here
+    const array = [1, 2, 3];
+    expect(_.contains(array, 2)).toBe(true);
   });
 
   it('returns false if an array does not contain the target', () => {
-    // Your code goes here
+    const array = [1, 2, 3];
+    expect(_.contains(array, 4)).toBe(false);
   });
 
   it('returns true if the target value is among the values of an object', () => {
-    // Your code goes here
+    const object = {
+    name: 'sunny',
+    age: 2 
+    };
+    expect(_.contains(object, 2)).toBe(true);
   });
 
   it('returns false if the target value is not among the values of an object', () => {
-    // Your code goes here
+    const object = {
+    name: 'sunny',
+    age: 2 
+    };
+    expect(_.contains(object, 4)).toBe(false);
   });
 
 });
